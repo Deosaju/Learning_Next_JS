16d938214ba40c362f1cad7b7dbc4f46f8db992f
deosaju <DEOSAJU2260@GMAIL.COM>
2023-03-03 21:46:21 +0530

    Course done

--- components/header.js
+++ components/header.js
@@ -1,7 +1,22 @@
+import {signIn,signOut} from 'next-auth/next'
+import Link from 'next/link'
+
 function Header(){
     return (
         <div className="layout-header">
             <h1>My Header</h1>
+            <br></br>
+            <Link href='/api/auth/signin' style={{color:'red'}} onClick={e=>{
+                e.preventDefault()
+            }}>
+                Sign In
+            </Link>
+            <br></br>
+            <Link href='/api/auth/signout' style={{color:'red'}} onClick={e=>{
+                e.preventDefault()
+            }}>
+                Sign Out
+            </Link>
         </div>
     )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -10,9 +10,9 @@ function contact() {
                 <title>About</title>
                 <meta name="keywords" content="about" />
             </Head>
-
+            This is from environment {process.env.NEXT_PUBLIC_API_URL}
             <h2 className={styles.highlightscss} >About</h2>
-            <Image src="/Readme_assets/Routing.png" width={200} height={200} />
+            <Image src="/Readme_assets/Routing.png" width={200} height={200} alt='bigi'/>
         </>
     )
 }
@@ -27,4 +27,20 @@ contact.getLayout = function getLayout(page) {
             <Footer /> 
         </>
     )
+}
+
+
+export const getStaticProps = async () => {
+    const user = process.env.DB_USERNAME;
+    const pass = process.env.DB_PASSWORD;
+
+    console.log(user, pass);
+
+    return {
+        props: {
+            user,
+            pass,
+        },
+    };
+
 }
