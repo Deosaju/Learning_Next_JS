e3631bdfaeac939a42d4e40cd9d3a2c81ac3f418
deosaju <DEOSAJU2260@GMAIL.COM>
2023-03-03 09:36:51 +0530

    CSS - SASS,Inline,Global

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
 }
